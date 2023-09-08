7fcc06b4b31d26ef10f28aadace0ed111ade7f56
Oleh Pona <lanuoleg@gmail.com>
2023-09-08 23:13:53 +0300

    Added tabs change script for fresh deals.

--- scripts/fresh-deals.js
+++ scripts/fresh-deals.js
@@ -2,13 +2,24 @@ function ChangeSelectedTab(id){
     const elements = document.querySelectorAll(".fresh_deals__main__tabs__element");
     Array.from(elements).map(el => {
         if (el.id === id){
-            el.classList.add("selected")
+            el.classList.add("selected");
+            ChangeTab(el.id)
         } else {
-            el.classList.remove("selected")
+            el.classList.remove("selected");
         }
     })
 }
-
+function ChangeTab(id){
+    const tab_id = "tab_" + id.split("-")[1];
+    const elements = document.querySelectorAll(".fresh_deals__tab");
+    Array.from(elements).map(el => {
+        if (el.id === tab_id){
+            el.style.display = "block";
+        } else {
+            el.style.display = "None";
+        }
+    })
+}
 // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⡟⠀⠀⠀⠀⠀⠀⠀
 // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⣌⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⣡⡶⡇⠀⠀⠀⠀⠀⠀⠀
 // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⢻⡌⠳⣄⠉⠳⢤⠴⠒⠛⠛⠛⠛⠒⠦⢤⡤⠚⣡⠞⢁⣿⡇⠀⠀⠀⠀⠀⠀⠀
