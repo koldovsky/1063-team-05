5266117b39cf555d902013c66e324511c3b27c7a
Oleh Pona <lanuoleg@gmail.com>
2023-09-13 17:43:18 +0300

    Fresh deals js function name fix

--- scripts/fresh-deals.js
+++ scripts/fresh-deals.js
@@ -1,15 +1,15 @@
-function ChangeSelectedTab(id){
+function changeSelectedTab(id){
     const elements = document.querySelectorAll(".fresh_deals__main__tabs__element");
     Array.from(elements).map(el => {
         if (el.id === id){
             el.classList.add("selected");
-            ChangeTab(el.id)
+            changeTab(el.id)
         } else {
             el.classList.remove("selected");
         }
     })
 }
-function ChangeTab(id){
+function changeTab(id){
     const tab_id = "tab_" + id.split("-")[1];
     const elements = document.querySelectorAll(".fresh_deals__tab");
     Array.from(elements).map(el => {
@@ -20,15 +20,4 @@ function ChangeTab(id){
         }
     })
 }
-
-function Secret(){
-    document.querySelector("body").innerHTML = `
-    <iframe width="1280" height="720" src="https://www.youtube.com/embed/YRvOePz2OqQ?si=NY-0gjiraXoHKFk7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen autoplay="1" controls="0"></iframe>
-    `
-}
-//          █▀▀▀▄            █▀▀▀▄
-//    █     █   █    ▄▀▀▀▄   █   █      █
-//   █ █    █▀▀▀▄    █   █   █▀▀▀▄     █ █
-//  █===█   █   █    █   █   █   █    █===█
-// █     █  █▄▄▄█    ▀▄▄▄▀   █▄▄▄█   █     █
 
