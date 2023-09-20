4044439f7c47e36f626f0828d53d906c6e8bc2d3
Vitalii Lozovan <vitalulozovan@gmail.com>
2023-09-20 17:11:36 +0300

    Update tasks-05.js

--- js-tasks/tasks-05.js
+++ js-tasks/tasks-05.js
@@ -35,28 +35,5 @@ function lovefunc(flower1, flower2) {
   return flower1 % 2 !== flower2 % 2;
 }
 
-// Convert a string to an array https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-function stringToArray(string) {
-  return string.split(" ");
-}
-// DNA to RNA Conversion https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
-function DNAtoRNA(dna) {
-  return (rna = dna.replace(/T/g, "U"));
-}
-// Find Maximum and Minimum Values of a List https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-const min = function (list) {
-  return Math.min.apply(null, list);
-};
-const max = function (list) {
-  return Math.max.apply(null, list);
-};
-// Smallest value of an array https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
-function min(arr, toReturn) {
-  if (toReturn === "value") {
-    return Math.min(...arr);
-  }
-  if (toReturn === "index") {
-    return arr.indexOf(Math.min(...arr));
-  }
-}
+
 
