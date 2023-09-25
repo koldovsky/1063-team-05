ad90b49b5f876f629a2fd012b9fb87abdf92ce2a
Dmitriy <mytrofann@gmail.com>
2023-09-25 16:53:02 +0300

    Corrects js in top-picks

--- scripts/top-picks.js
+++ scripts/top-picks.js
@@ -1,4 +1,3 @@
-// Kravchuk Dmytro
 const products = [
     {
         id: 1,
@@ -37,24 +36,46 @@ const products = [
     }
 ];
 
-function renderProducts(products) {
+let currentIndex = 0;
+
+function renderProducts(startIndex) {
+    // Wrap the product list so that the first and last products are adjacent
+    const wrappedProducts = [...products, ...products.slice(0, startIndex)];
+
     let productsHtml = "";
-    for(const product of products) {
-        productsHtml += 
-        `<li class="top-picks__item">
-        <article class="top-picks__card">
-            <img class="top-picks__card-img" src="${product.imageUrl}" alt="${product.name}">
-            <a class="top-picks__card-link" href="#">${product.name}</a>
-            <p class="top-picks__card-price">${product.price}<span class="top-picks__product-currency">${product.curency}</span></p>
-            <div class="top-picks__card-button-button-wrapper">
-                <button class="top-picks__card-button">Buy Now</button>
-            </div>
-        </article>
-    </li>`
-        
+    for (let i = startIndex; i < startIndex + 4; i++) {
+        const product = wrappedProducts[i];
+        productsHtml += `
+            <li class="top-picks__item">
+                <article class="top-picks__card">
+                    <img class="top-picks__card-img" src="${product.imageUrl}" alt="${product.name}">
+                    <a class="top-picks__card-link" href="#">${product.name}</a>
+                    <p class="top-picks__card-price">${product.price}<span class="top-picks__product-currency">${product.curency}</span></p>
+                    <div class="top-picks__card-button-button-wrapper">
+                        <button class="top-picks__card-button">Buy Now</button>
+                    </div>
+                </article>
+            </li>`;
     }
     document.querySelector(".top-picks__list").innerHTML = productsHtml;
 }
-renderProducts(products);
-            
-            
+
+function scrollProducts(direction) {
+    if (direction === 'next') {
+        currentIndex = (currentIndex + 1) % products.length;
+    } else if (direction === 'prev') {
+        currentIndex = (currentIndex - 1 + products.length) % products.length;
+    }
+    renderProducts(currentIndex);
+}
+
+document.querySelector('.top-picks__button-prev').addEventListener('click', () => {
+    scrollProducts('prev');
+});
+
+document.querySelector('.top-picks__button-next').addEventListener('click', () => {
+    scrollProducts('next');
+});
+
+// Render the initial set of products
+renderProducts(currentIndex);
