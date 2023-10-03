d5198b89ed15f3ca30bf30739f8ebd79ce896383
Vitalii Lozovan <vitalulozovan@gmail.com>
2023-10-03 23:03:16 +0000

    footer change css and check form send valid

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ document.addEventListener('partialsLoaded', () => {
     import("./header.js");
     import("./fresh-deals.js");
     import("./top-picks.js")
-    import("./organic.js");
+    import("./organic.js");      
     import("./countdown.js");
     import("./partners-carousel.js")
 });
